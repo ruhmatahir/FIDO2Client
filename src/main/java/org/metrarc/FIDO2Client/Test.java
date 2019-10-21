9a09af6ee27b714f7e56e913ed13b0610a3a64fd
ruhmatahir <ruhma.mail@gmail.com>
2019-10-21 18:14:08 +0100

    Test for PIN input

---
+++ src/main/java/org/metrarc/FIDO2Client/Test.java
@@ -0,0 +1,35 @@
+package org.metrarc.FIDO2Client;
+
+import java.io.*;
+
+public class Test {
+    public static void main(String[] args) {
+        try {
+            String s;
+            ProcessBuilder pb = new ProcessBuilder("sh", "-c", "ssh", "ali@localhost", ">/dev/tty");
+            pb.redirectInput(ProcessBuilder.Redirect.INHERIT);
+            pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+
+            Process ssh = pb.start();
+
+//            BufferedReader stdOut = new BufferedReader(new InputStreamReader (ssh.getInputStream()));
+            OutputStream stdIn = ssh.getOutputStream ();
+            stdIn.write ("ali\n".getBytes ("US-ASCII"));
+            stdIn.flush();
+            stdIn.close();
+
+//            while ((s = stdOut.readLine()) != null)
+//                System.out.println("Line: " + s);
+//
+//            if(s==null)
+//                System.out.println("No FIDO device detected");
+//
+//            stdIn.write ("ali\n".getBytes ("US-ASCII"));
+//            stdIn.flush ();
+
+            ssh.waitFor();
+            System.out.println ("Exit: " + ssh.exitValue());
+            ssh.destroy();
+        } catch (Exception e) { e.printStackTrace(); }
+    }
+}
